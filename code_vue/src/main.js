a3618336e2c68f63ee43e4eeebd3357b2c94fff2
小树林 <2691296979@qq.com>
2020-05-19 10:02:46 +0800

    20/5/19 侧边栏导航完成

--- code_vue/src/main.js
+++ code_vue/src/main.js
@@ -8,11 +8,18 @@ import './assets/css/global.css'
 // 导入 element-ui
 import './plugins/element.js'
 // 导入icon图标
-import './assets/font_1822600_omucaroikvc/iconfont.css'
+import './assets/font_1822600_ejbcxjhmgo4/iconfont.css'
 // 导入 axios
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// axios请求拦截
+axios.interceptors.request.use(config => {
+  // 为请求头对象，添加 token 验证的 Authorization 字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
 // 挂载到Vue的原型上
 Vue.prototype.$http = axios
 
--- code_vue/src/plugins/element.js
+++ code_vue/src/plugins/element.js
@@ -1,9 +1,31 @@
 import Vue from 'vue'
-import {Button, Form, FormItem, Input, Message} from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Header,
+  Aside,
+  Main,
+  Footer,
+  Container,
+  Menu,
+  Submenu,
+  MenuItem
+} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Footer)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
 // 这个弹框提示必须使用原型链
 Vue.prototype.$message = Message
